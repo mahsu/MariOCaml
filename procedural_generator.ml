6400c2ca1a6dc27f60467364fa724d5e6efdfe91
Ashley Xue <ax25@cornell.edu>
2015-12-02 01:11:19 -0500

    ground sprite sheet and starting generation

---
+++ procedural_generator.ml
@@ -0,0 +1,20 @@
+(*Set boundary variables*)
+(*Max height Mario can jump*)
+let max_jump = 3;;
+(*Max distance Mario can jump*)
+let max_dist = 3;;
+(*Height of ground based on number of grids*)
+let ground_height = 2;;
+
+(*Canvas is 480 by 320 (w*h) -> 30 by 20 blocks
+* Let the first generaed map just be of size 5 by 5 blocks *)
+
+
+
+(*Procedurally generates a new map of default size
+* Return a list (maybe array) holding all the objects generated on the canvas
+* Each object should hold a (record?) containing the coordinates and type
+* TEMP
+*)
+let generate () = failwith "TODO"
+
